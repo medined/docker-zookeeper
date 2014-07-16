dc2a86564a682ec9a0d60a68b0fb545e77119ded
David <david.medinets@gmail.com>
2014-07-15 23:08:32 -0400

    Use pipework to wait for bridge network interface

--- zookeeper/start-zookeeper.sh
+++ zookeeper/start-zookeeper.sh
@@ -5,6 +5,8 @@ NODEID=$1
 echo "Node $NODEID reporting for duty!"
 echo $NODEID > /zookeeper/data/myid
 
-sleep 30
+IPADDR="10.0.10.${NODEID}"
+
+/pipework --wait br1 zookeeper.ui  "$IPADDR/24"
 
 /opt/zookeeper-3.4.6/bin/zkServer.sh start-foreground
